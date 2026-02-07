495c48d10e7558a31b5e70744354940cbe5d4e25
Mujahid137 <mdjx137@gmail.com>
2026-02-07 17:32:14 +0600

    Fix Vite base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const isVercel = process.env.VERCEL === "1";
+
 export default defineConfig({
-  base: '/basketworks/',
+  base: isVercel ? "/" : "/basketworks/",
   plugins: [react()],
 })
